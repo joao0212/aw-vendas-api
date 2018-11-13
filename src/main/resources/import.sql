48409fe198845604a6fae8165f54f355438d6aa0
João Victor <jvc.martins12@gmail.com>
2018-11-13 10:59:53 -0200

    Alteração no arquivo import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,7 @@
 insert into cliente (id, nome) values (1, 'João Victor');
 
 insert into produto (id, nome, valor) values (1, 'Notebook Dell', 3000.0)
+insert into produto (id, nome, valor) values (2, 'Desktop Sony', 4000.0)
 
 insert into venda (id, cadastro, cliente_id, frete, total) values (1, sysdate(), 1, 15.0, 3015.0);
 
